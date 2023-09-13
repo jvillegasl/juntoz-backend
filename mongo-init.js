845b8943ffbdd5c1bc27b309a536a2ab36688943
jvillegasl <jvillegasl@uni.pe>
2023-09-12 19:04:40 -0500

    mongo init seed implemented

--- mongo-init.js
+++ mongo-init.js
@@ -8,3 +8,21 @@ db.createUser({
         },
     ],
 });
+
+db = new Mongo().getDB("juntoz");
+
+db.createCollection("orders", { capped: false });
+
+const ITEMS_COUNT = 20;
+
+var items = [];
+
+for (let i = 0; i < ITEMS_COUNT; i++) {
+    const OrderNumber = i;
+    const ClientDNI = "0000000" + String(Math.floor(Math.random() * 9));
+    const Description = `Description N°${i}`;
+
+    items.push({ OrderNumber, ClientDNI, Description });
+}
+
+db.orders.insert(items);
